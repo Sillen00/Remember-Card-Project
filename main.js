0078c6e19ca1411e0a2e0d6a9e56492c261776a6
Simon Bengtsson <sillen.00@hotmail.com>
2022-12-28 20:27:23 +0100

    Added images

--- main.js
+++ main.js
@@ -2,5 +2,17 @@ document.addEventListener("DOMContentLoaded", main)
 
 
 function main(){
-    
+    cardsFunction();
+}
+
+
+cardsFunction(){
+    const cards = [
+        {
+            name: "fries"
+            img: ""
+        }
+    ]    
+
+
 }
