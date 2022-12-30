0eccb6446924639ce527bb1161589db0c94da490
Simon Bengtsson <sillen.00@hotmail.com>
2022-12-30 22:13:32 +0100

    create card objekts and function for render blank cards

--- main.js
+++ main.js
@@ -1,18 +1,105 @@
-document.addEventListener("DOMContentLoaded", main)
+document.addEventListener("DOMContentLoaded", main);
 
-
-function main(){
-    cardsFunction();
+function main() {
+  cardsFunction();
 }
 
+function cardsFunction() {
+  const cards = [
+    {
+      name: "anime",
+      img: "./images/Anime.png",
+    },
+    {
+      name: "apple",
+      img: "./images/Apple.png",
+    },
+    {
+      name: "avocado",
+      img: "./images/Avocado.png",
+    },
+    {
+      name: "car",
+      img: "./images/Car.png",
+    },
+    {
+      name: "dog",
+      img: "./images/Dog.png",
+    },
+    {
+      name: "iron-man",
+      img: "./images/Iron Man.png",
+    },
+    {
+      name: "pixels",
+      img: "./images/Pixels.png",
+    },
+    {
+      name: "tank",
+      img: "./images/Tank.png",
+    },
+    {
+      name: "troll",
+      img: "./images/Troll.png",
+    },
+    {
+      name: "anime",
+      img: "./images/Anime.png",
+    },
+    {
+      name: "apple",
+      img: "./images/Apple.png",
+    },
+    {
+      name: "avocado",
+      img: "./images/Avocado.png",
+    },
+    {
+      name: "car",
+      img: "./images/Car.png",
+    },
+    {
+      name: "dog",
+      img: "./images/Dog.png",
+    },
+    {
+      name: "iron-man",
+      img: "./images/Iron Man.png",
+    },
+    {
+      name: "pixels",
+      img: "./images/Pixels.png",
+    },
+    {
+      name: "tank",
+      img: "./images/Tank.png",
+    },
+    {
+      name: "troll",
+      img: "./images/Troll.png",
+    },
+  ];
+  cards.sort(() => 0.5 - Math.random());
+  console.log(cards);
 
-cardsFunction(){
-    const cards = [
-        {
-            name: "fries"
-            img: ""
-        }
-    ]    
+  createBoard(cards);
 
+  function createBoard(cards) {
+    const grid = document.querySelector(".grid");
+    for (let i = 0; i < cards.length; i++) {
+      const card = document.createElement("img");
+      card.setAttribute("src", "./images/blank.jpg");
+      card.setAttribute("data-id", i);
+      grid.appendChild(card);
+      card.addEventListener("click", flipCard);
+    }
+  }
 
+  let cardsChosen = [];
+
+  function flipCard() {
+    let cardID = this.getAttribute("data-id");
+    console.log(cards[cardID].name);
+    cardsChosen.push(cards[cardID].name);
+  }
 }
